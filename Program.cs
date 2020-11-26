8209c53025aebc46453ae9bb5264c95f02c55f91
Blagovest-Todorov <blagovest.stod@abv.bg>
2020-11-26 20:24:25 +0200

    init

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace _06.matrixMultiplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int [,] first =   
+            { 
+                  { 1, 2, 3 },
+                  { 4, 5, 6}
+            };
+            int[,] second = 
+            {
+                {7, 8 },
+                {9, 10},
+                {11, 12}
+            };
+
+            int rows = first.GetLength(0);
+            int cols = second.GetLength(1);
+
+            int[,] result = new int[rows, cols];
+
+            for (int row = 0; row < result.GetLength(0); row++)
+            {
+                for (int col = 0; col < result.GetLength(1); col++)
+                {
+                    int currentResult = 0;
+                    for (int element = 0; element < first.GetLength(1); element++)
+                    {
+                      currentResult += first[row, element] * second[element, col];
+                    }
+                    result[row, col] = currentResult;
+                }
+            }
+            foreach (int item in result)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
